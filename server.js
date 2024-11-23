3f78ac4ecff0340849d24ee2d6d5d0595b466c57
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2024-11-22 21:03:38 -0300

    5ª Aula e Deploy do Projeto

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import routes from "./src/routes/postRoutes.js";
 
 // Cria uma instância do aplicativo Express
 const app = express();
+app.use(express.static("uploads"));
 routes(app);
 
 // Inicializa o servidor na porta 3000
